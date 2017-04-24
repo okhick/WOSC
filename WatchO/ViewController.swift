1f694bc35c26d0c9ea64408d9cebc6148b66b16f
Oliver Hickman <okhick@gmail.com>
2017-04-24 14:46:21 -0400

    Initial Commit

---
+++ WatchO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchO
+//
+//  Created by Oliver Hickman on 4/24/17.
+//  Copyright © 2017 Oliver Hickman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
